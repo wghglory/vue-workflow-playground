8f3f2c6a46b4411cc49e8433f4b352a86646d1f3
Derek Wang <guanghuiw@vmware.com>
2021-11-17 20:17:12 +0800

    chores: add naive-ui

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+import { NConfigProvider, NInput, NDatePicker, NSpace } from 'naive-ui';
+// locale & dateLocale
+import { enUS, dateEnUS, darkTheme } from 'naive-ui';
+
+// https://www.naiveui.com/en-US/dark/docs/theme
+const theme = ref(null); // darkTheme
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <n-config-provider :theme="theme" :locale="enUS" :date-locale="dateEnUS">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </n-config-provider>
 </template>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import 'vfonts/Lato.css';
+
 createApp(App).mount('#app');
