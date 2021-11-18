8295a4ba7c7c196c6c6ae599e882e22ec4f24af1
Derek Wang <guanghuiw@vmware.com>
2021-11-18 12:21:03 +0800

    feat: adjust layout

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,23 @@ import { ref } from 'vue';
 // import HelloWorld from './components/HelloWorld.vue';
 import TheHeader from './components/TheHeader.vue';
 
-import { NConfigProvider, NInput, NDatePicker, NSpace } from 'naive-ui';
+import { NConfigProvider, NLayout } from 'naive-ui';
 // locale & dateLocale
 import { enUS, dateEnUS, darkTheme } from 'naive-ui';
 
 // https://www.naiveui.com/en-US/dark/docs/theme
-const theme = ref(null); // darkTheme
+// const theme = ref(null); // darkTheme
+const theme = ref(darkTheme); // darkTheme
 </script>
 
 <template>
-  <n-config-provider :theme="theme" :locale="enUS" :date-locale="dateEnUS">
-    <TheHeader />
-    <main class="p-6">
-      <router-view></router-view>
-    </main>
+  <n-config-provider :theme="theme" :locale="enUS" :date-locale="dateEnUS" class="h-full">
+    <n-layout content-style="display:flex;flex-direction:column;" embedded>
+      <TheHeader />
+      <main class="p-6 flex-1">
+        <router-view></router-view>
+      </main>
+    </n-layout>
   </n-config-provider>
 </template>
 
@@ -32,7 +35,7 @@ body {
   height: 100vh;
 }
 
-#app {
-  height: 100%;
+.n-layout .n-layout-scroll-container {
+  height: 100vh;
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,15 @@
 <template>
-  <header>
-    <nav>
-      <n-menu v-model:value="activeKey" :options="menuOptions" mode="horizontal" />
+  <header class="flex items-center py-2 px-4 bg-gray-600 text-white">
+    <h1 class="pr-6">
+      <router-link to="/" class="flex items-center">
+        <n-icon size="40">
+          <cloud-outline />
+        </n-icon>
+        <span class="font-bold text-2xl pl-3">PCDL</span>
+      </router-link>
+    </h1>
+    <nav class="flex-1">
+      <n-menu v-model:value="activeKey" :inverted="true" :options="menuOptions" mode="horizontal" />
       <!-- <router-link to="/">Go to Home</router-link>
     <router-link to="/about">Go to About</router-link> -->
     </nav>
@@ -12,11 +20,13 @@
 import { h, ref } from 'vue';
 import { RouterLink } from 'vue-router';
 import { NIcon, NMenu } from 'naive-ui';
+
 import {
   BookOutline as BookIcon,
   PersonOutline as PersonIcon,
   WineOutline as WineIcon,
   HomeOutline as HomeIcon,
+  CloudOutline,
 } from '@vicons/ionicons5';
 
 function renderIcon(icon: any) {
