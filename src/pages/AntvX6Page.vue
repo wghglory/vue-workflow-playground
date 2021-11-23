4f33577bac07edf84cfcd681e4b3ba58affec0eb
Derek Wang <guanghui-wang@foxmail.com>
2021-11-23 15:49:32 +0800

    feat: add x6

---
+++ src/pages/AntvX6Page.vue
@@ -0,0 +1,110 @@
+<template>
+  <div ref="container" class="h-full"></div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, onMounted, onUnmounted } from 'vue';
+import { Graph } from '@antv/x6';
+
+const data: any = reactive({
+  nodes: [
+    {
+      id: 50,
+      shape: 'rect',
+      x: 100,
+      y: 150,
+      width: 80,
+      height: 40,
+      label: '你好',
+      attrs: {
+        body: {
+          fill: '#2ECC71',
+          stroke: '#000',
+          strokeDasharray: '10,2',
+        },
+        label: {
+          text: 'Hello',
+          fill: '#333',
+          fontSize: 13,
+        },
+      },
+    },
+    {
+      id: 21,
+      // shape: 'circle',
+      x: 300,
+      y: 150,
+      width: 80,
+      height: 40,
+      label: 'world',
+      attrs: {
+        body: {
+          fill: '#F39C12',
+          stroke: '#000',
+          rx: 16,
+          ry: 16,
+        },
+        label: {
+          text: 'World',
+          fill: '#333',
+          fontSize: 18,
+          fontWeight: 'bold',
+          fontVariant: 'small-caps',
+        },
+      },
+    },
+  ],
+  edges: [
+    {
+      source: 50,
+      target: 21,
+      attrs: {
+        line: {
+          stroke: 'orange',
+        },
+      },
+    },
+  ],
+});
+
+const container = ref(null);
+const lf: any = reactive({});
+
+onMounted(() => {
+  lf.value = new Graph({
+    panning: true,
+    container: container.value!,
+    background: {
+      color: '#fffbe6', // 设置画布背景颜色
+    },
+    grid: {
+      size: 10, // 网格大小 10px
+      visible: true, // 渲染网格背景
+    },
+    // grid: false,
+    // background: {
+    //   color: '#F5FBFD',
+    // },
+    keyboard: {
+      enabled: true,
+    },
+  });
+
+  const lfIstance = lf.value;
+  // lfIstance.centerContent();
+
+  // export svg
+  // lfIstance.toSVG((dataUri: string) => {
+  //   // 下载
+  //   DataUri.downloadDataUri(DataUri.svgToDataUrl(dataUri), 'chart.svg');
+  // });
+
+  lfIstance.fromJSON(data);
+});
+
+onUnmounted(() => {
+  lf.value.dispose();
+});
+</script>
+
+<style scoped></style>
